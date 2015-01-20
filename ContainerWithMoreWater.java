6ad0a6f71bd807ef3d59644ed995a5c2e9fc8b55
liushaobo <liushaobot@163.com>
2015-01-20 11:52:12 +0800

    Update ContainerWithMoreWater.java

--- ContainerWithMoreWater.java
+++ ContainerWithMoreWater.java
@@ -1,5 +1,7 @@
 /*
-Given n non-negative integers a1, a2, ..., an, where each represents a point at coordinate (i, ai). n vertical lines are drawn such that the two endpoints of line i is at (i, ai) and (i, 0). Find two lines, which together with x-axis forms a container, such that the container contains the most water.
+Given n non-negative integers a1, a2, ..., an, where each represents a point at coordinate (i, ai). n vertical lines are
+drawn such that the two endpoints of line i is at (i, ai) and (i, 0). Find two lines, which together with x-axis forms 
+a container, such that the container contains the most water.
 Note: You may not slant the container.
 贪心算法旨在尽可能找到两条距离较远且高度较高的线作为边。从两端开始扫描，当左边的线较短时，只有右移左边的线才可能找到更优的解决方案
 ；同理，当右边的线较短时，只有左移右边的线才可能找到更优的方案。
