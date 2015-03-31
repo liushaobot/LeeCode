4c9ee80b92754c7ee8462e036a5f376410c69090
liushaobo <liushaobot@163.com>
2015-03-31 14:44:23 +0800

    Create RotateList.java

---
+++ RotateList.java
@@ -0,0 +1,45 @@
+/*
+Given a list, rotate the list to the right by k places, where k is non-negative.
+
+For example:
+Given 1->2->3->4->5->NULL and k = 2,
+return 4->5->1->2->3->NULL.
+*/
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public ListNode rotateRight(ListNode head, int k) {
+        if (k == 0 || head == null || head.next == null)
+            return head;
+        ListNode p = head, q = head;
+        int length = 0;
+        while (q != null){
+            ++length;
+            q = q.next;
+        }
+        if (k % length == 0)
+            return head;
+        k = k % length;
+        q = head;
+        while (k-- != 0){
+            q = q.next;
+        }
+        while (q.next != null){
+            p = p.next;
+            q = q.next;
+        }
+        ListNode res = p.next;
+        p.next = null;
+        q.next = head;
+        return res;
+    }
+}
