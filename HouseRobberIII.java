80920548590338f3e6bc4adabf360e105a516898
liushaobo <liushaobot@163.com>
2016-03-20 19:35:10 +0800

    Update HouseRobberIII.java

--- HouseRobberIII.java
+++ HouseRobberIII.java
@@ -19,6 +19,8 @@ Example 2:
   / \   \ 
  1   3   1
 Maximum amount of money the thief can rob = 4 + 5 = 9.
+
+Reference : https://leetcode.com/discuss/91899/step-by-step-tackling-of-the-problem
 */
 
 /**
