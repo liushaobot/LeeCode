3bea15abd22f0ed668ea4f91f64ede6eebe46477
liushaobo <liushaobot@163.com>
2016-03-28 22:00:06 +0800

    Update ContainsDuplicateIII.java

--- ContainsDuplicateIII.java
+++ ContainsDuplicateIII.java
@@ -18,4 +18,17 @@ public class Solution {
         }
         return false;
     }
+    
+    if(nums.length <= 1 || t < 0 || k < 1) {return false;}
+        TreeSet<Long> set = new TreeSet<Long>();
+        for(int i = 0; i < nums.length; i++){
+            long min = Math.min((long)nums[i] - t,(long)nums[i] + t + 1);
+            long max = Math.max((long)nums[i] - t, (long)nums[i] + t + 1);
+        /*1.if the subset is not empty, means that we have the element that satisfy the requirement 
+          2.if we cannot add the element to the set, that means we already have the element*/
+            if(!set.subSet(min,max).isEmpty() || !set.add((long)nums[i])) {return true;}
+            set.add((long)nums[i]);
+            if(i >= k) {set.remove((long)nums[i - k]);}
+        }
+        return false;
 }
