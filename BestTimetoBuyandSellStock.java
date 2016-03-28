d749b52f6bf1671f713556f337d9d2e599883ede
liushaobo <liushaobot@163.com>
2016-03-28 11:05:54 +0800

    Update BestTimetoBuyandSellStock.java

--- BestTimetoBuyandSellStock.java
+++ BestTimetoBuyandSellStock.java
@@ -3,6 +3,7 @@ Say you have an array for which the ith element is the price of a given stock on
 
 If you were only permitted to complete at most one transaction (ie, buy one and sell one share of the stock), 
 design an algorithm to find the maximum profit.
+Dynamic Programming
 */
 
 public class Solution {
