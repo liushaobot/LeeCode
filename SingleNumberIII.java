26121abe929fb1d1169329b90a3e310ed20a89b3
liushaobo <liushaobot@163.com>
2016-03-29 19:37:06 +0800

    Update file SingleNumberIII.java

--- SingleNumberIII.java
+++ SingleNumberIII.java
@@ -4,8 +4,8 @@
 public class Solution {
     public int[] singleNumber(int[] nums) {
         int sum = 0;
-        for (int i = 0; i < nums.length; ++i) {
-            sum ^= nums[i];
+        for (int num : nums) {
+            sum ^= num;
         }
 
         //int bit = 0;
@@ -13,11 +13,11 @@ public class Solution {
         int bit = (sum&(sum-1)) ^ sum;
 
         int[] result = new int[2];
-        for (int i = 0; i < nums.length; ++i) {
-            if ((nums[i]&bit) == 0) {//if ((nums[i]&(1<<bit)) == 0) {
-                result[0] ^= nums[i];
+        for (int num : nums) {
+            if ((num&bit) == 0) {//if ((num&(1<<bit)) == 0) {
+                result[0] ^= num;
             } else {
-                result[1] ^= nums[i];
+                result[1] ^= num;
             }
         }
         return result;
