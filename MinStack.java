6f8361e649a4417e803bed998b20b8c2febbaab0
liushaobo <liushaobot@163.com>
2016-04-14 15:40:54 +0800

    update MinStack.java

--- MinStack.java
+++ MinStack.java
@@ -7,6 +7,25 @@
     getMin() -- Retrieve the minimum element in the stack.
 */
 class MinStack {
+    Stack<Integer> stack=new Stack<>();
+    int min=Integer.MAX_VALUE;
+    public void push(int x) {
+        if(x<=min) {stack.push(min); min=x;}
+        stack.push(x);
+    }
+    public void pop() {
+        if(stack.peek()==min){ stack.pop(); min=stack.pop(); }
+        else stack.pop();
+    }
+    public int top() {
+        return stack.peek();
+    }
+    public int getMin() {
+        return min;
+    }
+    
+    ///////////////////////////////////////////////////////////////////////////
+    
     private List<Integer> stack = new ArrayList<Integer>();
     private List<Integer> min = new ArrayList<Integer>();
     private List<Integer> nMin = new ArrayList<Integer>();
