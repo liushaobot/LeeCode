b0fefc90e9f17f3806058abb846141961a1853da
liushaobo <liushaobot@163.com>
2016-04-14 16:00:10 +0800

    update BitwiseANDofNumbersRange.java

--- BitwiseANDofNumbersRange.java
+++ BitwiseANDofNumbersRange.java
@@ -6,7 +6,19 @@ For example, given the range [5, 7], you should return 4.
 
 public class Solution {
     public int rangeBitwiseAnd(int m, int n) {
+        if(m == 0){
+            return 0;
+        }
+        int moveFactor = 1;
+        while(m != n){
+            m >>= 1;
+            n >>= 1;
+            moveFactor <<= 1;
+        }
+        return m * moveFactor;
+        /*
         int t = (int) Math.ceil(Math.log(n-m+1)/Math.log(2));
         return m&n >> t << t;
+        */
     }
 }
