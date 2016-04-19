a3ce38617a35a9fca970a229f4543df6217b8420
liushaobo <liushaobot@163.com>
2016-04-19 10:00:27 +0800

    update RepeatedDNASequences.java

--- RepeatedDNASequences.java
+++ RepeatedDNASequences.java
@@ -13,6 +13,28 @@ Return:
 
 public class Solution {
     public List<String> findRepeatedDnaSequences(String s) {
+        Set<Integer> words = new HashSet<Integer>();
+        Set<Integer> doubleWords = new HashSet<Integer>();
+        List<String> lists = new ArrayList<String>();
+        char[] map = new char[26];
+        //map['A' - 'A'] = 0;
+        map['C' - 'A'] = 1;
+        map['G' - 'A'] = 2;
+        map['T' - 'A'] = 3;
+
+        for(int i = 0; i < s.length() - 9; i++) {
+            int v = 0;
+            for(int j = i; j < i + 10; j++) {
+                v <<= 2;
+                v |= map[s.charAt(j) - 'A'];
+            }
+            if(!words.add(v) && doubleWords.add(v)) {
+                lists.add(s.substring(i, i + 10));
+            }
+        }
+        return lists;
+    }
+    public List<String> findRepeatedDnaSequences1(String s) {//by myself
         List<String> lists = new ArrayList<String>();
 
         if (s == null || s.length() <= 10)
