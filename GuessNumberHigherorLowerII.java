7a9981aa7f2c76b28f943e5107bc0a963e8b64fd
liushaobo <liushaobot@163.com>
2016-07-27 14:37:25 +0800

    GuessNumberHigherorLowerII.java

--- GuessNumberHigherorLowerII.java
+++ GuessNumberHigherorLowerII.java
@@ -62,4 +62,20 @@ public class Solution {
         }
         return dp[1][n];
     }
+
+    public int getMoneyAmount(int n) { //a little improvement :)
+        int[][] dp = new int[n+1][n+1];
+        for(int len=1;len<n;len++){
+            for(int i=1;i+len<=n;i++){
+                int j=i+len;
+                int min = Integer.MAX_VALUE;
+                for(int k=i;k<j;k++){
+                    int tmp = k+Math.max(dp[i][k-1],dp[k+1][j]);
+                    min = Math.min(min,tmp);
+                }
+                dp[i][j] = min;
+            }
+        }
+        return dp[1][n];
+    }
 }
