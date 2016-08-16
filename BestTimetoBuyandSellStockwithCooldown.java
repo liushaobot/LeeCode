f4b724f3fd111441264951845e19b7d58546b28a
liushaobo <liushaobot@163.com>
2016-08-16 14:37:50 +0800

    buy[i] = max(sell[i-2]-price, buy[i-1])

---
+++ BestTimetoBuyandSellStockwithCooldown.java
@@ -0,0 +1,30 @@
+/*
+Say you have an array for which the ith element is the price of a given stock on day i.
+
+Design an algorithm to find the maximum profit. You may complete as many transactions as you like (ie, buy one and sell one share of the stock multiple times) with the following restrictions:
+
+You may not engage in multiple transactions at the same time (ie, you must sell the stock before you buy again).
+After you sell your stock, you cannot buy stock on next day. (ie, cooldown 1 day)
+Example:
+
+prices = [1, 2, 3, 0, 2]
+maxProfit = 3
+transactions = [buy, sell, cooldown, buy, sell]
+
+buy[i] = max(sell[i-2]-price, buy[i-1])
+sell[i] = max(buy[i-1]+price, sell[i-1])
+*/
+
+public class Solution {
+    public int maxProfit(int[] prices) {
+        //https://discuss.leetcode.com/topic/30421/share-my-thinking-process
+        int sell = 0, prev_sell = 0, buy = Integer.MIN_VALUE, prev_buy;
+        for (int price : prices) {
+            prev_buy = buy;
+            buy = Math.max(prev_sell - price, prev_buy);
+            prev_sell = sell;
+            sell = Math.max(prev_buy + price, prev_sell);
+        }
+        return sell;
+    }
+}
