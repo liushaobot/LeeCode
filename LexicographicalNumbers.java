325520b96f50a03fefc13a889d1aad7e1e4e45ce
liushaobo <liushaobot@163.com>
2016-09-02 09:41:41 +0800

    Create LexicographicalNumbers.java

---
+++ LexicographicalNumbers.java
@@ -0,0 +1,26 @@
+/*
+Given an integer n, return 1 - n in lexicographical order.
+
+For example, given 13, return: [1,10,11,12,13,2,3,4,5,6,7,8,9].
+
+Please optimize your algorithm to use less time and space. The input size may be as large as 5,000,000.
+*/
+
+public class Solution {
+    public List<Integer> lexicalOrder(int n) {
+        List<Integer> res = new ArrayList<>();
+        for (int i = 1; i <= 9; ++i) {
+            helper(res, i, n);
+        }
+        return res;
+    }
+
+    private void helper(List<Integer> res, int cur, int n) {
+        if (cur <= n) {
+            res.add(cur);
+            for (int i = 0; i < 10; ++i) {
+                helper(res, cur*10+i, n);
+            }
+        }
+    }
+}
