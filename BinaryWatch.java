6cf30784f34713eb0dab129924ba1550f2068a37
liushaobo <liushaobot@163.com>
2016-09-21 16:31:27 +0800

    Update BinaryWatch.java

--- BinaryWatch.java
+++ BinaryWatch.java
@@ -54,4 +54,13 @@ public class Solution {
             }
         }
     }
+
+    public List<String> readBinaryWatch1(int num) {
+        List<String> times = new ArrayList<>();
+        for (int h=0; h<12; h++)
+            for (int m=0; m<60; m++)
+                if (Integer.bitCount(h * 64 + m) == num)
+                    times.add(String.format("%d:%02d", h, m));
+        return times;
+    }
 }
