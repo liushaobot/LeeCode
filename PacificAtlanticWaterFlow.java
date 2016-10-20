d7fd5c8181f438e7abe1eeb40d915fa51751ccb9
liushaobo <liushaobot@163.com>
2016-10-20 16:18:39 +0800

    Create PacificAtlanticWaterFlow.java

---
+++ PacificAtlanticWaterFlow.java
@@ -0,0 +1,120 @@
+/*
+Given an m x n matrix of non-negative integers representing the height of each unit cell in a continent, the "Pacific ocean" touches the left and top edges of the matrix and the "Atlantic ocean" touches the right and bottom edges.
+
+Water can only flow in four directions (up, down, left, or right) from a cell to another one with height equal or lower.
+
+Find the list of grid coordinates where water can flow to both the Pacific and Atlantic ocean.
+
+Note:
+The order of returned grid coordinates does not matter.
+Both m and n are less than 150.
+Example:
+
+Given the following 5x5 matrix:
+
+  Pacific ~   ~   ~   ~   ~ 
+       ~  1   2   2   3  (5) *
+       ~  3   2   3  (4) (4) *
+       ~  2   4  (5)  3   1  *
+       ~ (6) (7)  1   4   5  *
+       ~ (5)  1   1   2   4  *
+          *   *   *   *   * Atlantic
+
+Return:
+
+[[0, 4], [1, 3], [1, 4], [2, 2], [3, 0], [3, 1], [4, 0]] (positions with parentheses in above matrix).
+*/
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+/**
+ * Created by shaobo on 2016/10/20.
+ */
+public class Solution {
+    int[][]dir = new int[][]{{1,0},{-1,0},{0,1},{0,-1}};
+
+    public List<int[]> pacificAtlantic(int[][] matrix) {
+        List<int[]> res = new LinkedList<>();
+        if(matrix == null || matrix.length == 0 || matrix[0].length == 0){
+            return res;
+        }
+        int row = matrix.length, col = matrix[0].length;
+        boolean[][]pacific = new boolean[row][col];
+        boolean[][]atlantic = new boolean[row][col];
+        for(int i=0; i<row; i++){
+            dfs(matrix, pacific, Integer.MIN_VALUE, i, 0);
+            dfs(matrix, atlantic, Integer.MIN_VALUE, i, col-1);
+        }
+        for(int i=0; i<col; i++){
+            dfs(matrix, pacific, Integer.MIN_VALUE, 0, i);
+            dfs(matrix, atlantic, Integer.MIN_VALUE, row-1, i);
+        }
+        for (int i = 0; i < row; i++)
+            for (int j = 0; j < col; j++)
+                if (pacific[i][j] && atlantic[i][j])
+                    res.add(new int[] {i, j});
+        return res;
+    }
+
+
+    public void dfs(int[][]matrix, boolean[][]visited, int height, int x, int y){
+        int row = matrix.length, col = matrix[0].length;
+        if(x<0 || x>=row || y<0 || y>=col || visited[x][y] || matrix[x][y] < height)
+            return;
+        visited[x][y] = true;
+        for(int[]d:dir){
+            dfs(matrix, visited, matrix[x][y], x+d[0], y+d[1]);
+        }
+    }
+
+    public List<int[]> pacificAtlantic1(int[][] matrix) {
+        List<int[]> res = new LinkedList<>();
+        if(matrix == null || matrix.length == 0 || matrix[0].length == 0){
+            return res;
+        }
+        int row = matrix.length, col = matrix[0].length;
+        //One visited map for each ocean
+        boolean[][] pacific = new boolean[row][col];
+        boolean[][] atlantic = new boolean[row][col];
+        Queue<int[]> pQueue = new LinkedList<>();
+        Queue<int[]> aQueue = new LinkedList<>();
+        for(int i=0; i<row; i++){ //Vertical border
+            pQueue.offer(new int[]{i, 0});
+            aQueue.offer(new int[]{i, col-1});
+            pacific[i][0] = true;
+            atlantic[i][col-1] = true;
+        }
+        for(int i=0; i<col; i++){ //Horizontal border
+            pQueue.offer(new int[]{0, i});
+            aQueue.offer(new int[]{row-1, i});
+            pacific[0][i] = true;
+            atlantic[row-1][i] = true;
+        }
+        bfs(matrix, pQueue, pacific);
+        bfs(matrix, aQueue, atlantic);
+        for(int i=0; i<row; i++){
+            for(int j=0; j<col; j++){
+                if(pacific[i][j] && atlantic[i][j])
+                    res.add(new int[]{i,j});
+            }
+        }
+        return res;
+    }
+    public void bfs(int[][]matrix, Queue<int[]> queue, boolean[][]visited){
+        int row = matrix.length, col = matrix[0].length;
+        while(!queue.isEmpty()){
+            int[] cur = queue.poll();
+            for(int[] d:dir){
+                int x = cur[0]+d[0];
+                int y = cur[1]+d[1];
+                if(x<0 || x>=row || y<0 || y>=col || visited[x][y] || matrix[x][y] < matrix[cur[0]][cur[1]]){
+                    continue;
+                }
+                visited[x][y] = true;
+                queue.offer(new int[]{x, y});
+            }
+        }
+    }
+}
